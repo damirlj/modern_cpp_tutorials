396a64d01d245ece9c8861721947d9ccc4f3b64c
damirlj <damirlj@yahoo.com>
2021-10-09 11:19:28 +0200

    Tutorial 4 - Functional programming

---
+++ src/Tutorial 4/TestFP.hxx
@@ -0,0 +1,18 @@
+/*
+ * TestFP.hxx
+ *
+ *  Created on: Oct 4, 2021
+ *      Author: <a href="mailto:damirlj@yahoo.com">Damir Ljubic</a>
+ */
+
+#ifndef FP_TESTFP_HXX_
+#define FP_TESTFP_HXX_
+
+
+namespace test::fp
+{
+    int testFP();
+}
+
+
+#endif /* FP_TESTFP_HXX_ */
