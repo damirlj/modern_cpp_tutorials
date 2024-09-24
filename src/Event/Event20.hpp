406f3cbe58b3aff6f40568641e94a198997f201f
dalj8690 <damir.ljubic@esolutions.de>
2024-09-24 17:59:54 +0200

    Updated Event implementation - to work with broadcast signal

--- src/Event/Event20.hpp
+++ src/Event/Event20.hpp
@@ -1,33 +1,88 @@
+#ifndef EVENT20_HPP
+#define EVENT20_HPP
+
+// std library
+#include <chrono>
+#include <vector>
+#include <thread>
+
+// Application
 #include "Monitor.hpp"
 
 namespace utils
 {
     class Event final
     {
-        public:
+        using threads_ids_type = std::vector<std::thread::id>;
 
-            explicit Event(bool autoReset) noexcept : autoReset_{autoReset}
-            {}
+      public:
+        explicit Event(bool autoReset) noexcept
+            : autoReset_{autoReset}
+        {}
 
-            inline void wait() noexcept
+        ~Event() = default;
+        
+        // Copy functions forbidden
+
+        Event(const Event&) = delete;
+        Event& operator=(const Event&) = delete;
+
+        // Move operations forbidden
+
+        Event(Event&&) = delete;
+        Event& operator=(Event&&) = delete;
+
+        inline void wait() noexcept
+        {
             {
-                auto lock = sync_.wait([this] { return flag_; });
-                if (autoReset_) flag_ = false;
+                auto lock = sync_.getLock();
+                if (flag_) return;  // premature signalization
+                waitingThreads_.push_back(std::this_thread::get_id());
             }
+            auto lock = sync_.wait([this] { return flag_; });
+            update_waiting_threads(waitingThreads_);
+            if (autoReset_ && waitingThreads_.empty()) flag_ = false;
+        }
 
-            inline [[nodiscard]] bool wait_for(std::chrono::milliseconds timeout) noexcept
+        inline bool wait_for(std::chrono::milliseconds timeout) noexcept
+        {
             {
-                auto [result, lock] = sync_.wait_for(timeout, [this]{ return flag_; });
-                if (autoReset_) flag_ = false;
-                return result;
+                auto lock = sync_.getLock();
+                if (flag_) return true;  // premature signalization
+                waitingThreads_.push_back(std::this_thread::get_id());
             }
+            auto [result, lock] = sync_.wait_for(timeout, [this] { return flag_; });
+            update_waiting_threads(waitingThreads_);
+            if (autoReset_ && waitingThreads_.empty())
+                flag_ = false;  // for broadcast to work with auto reset flag set to true
 
-            inline void signal() noexcept { sync_.notify_one([this]{ flag_ = true; }); }
-            inline void broadcast() noexcept { sync_.notify_all([this] { flag_ = true; }); }
+            return result;
+        }
 
-        private:
-            const bool autoReset_;
-            bool flag_ {false};
-            Monitor<> sync_ {};
+        inline void signal() noexcept
+        {
+            sync_.notify_one([this] { flag_ = true; });
+        }
+
+        inline void broadcast() noexcept
+        {
+            sync_.notify_all([this] { flag_ = true; });
+        }
+
+      private:
+        inline void update_waiting_threads(threads_ids_type& waitingThreads) noexcept
+        {
+            waitingThreads_.erase(
+                std::remove(waitingThreads.begin(), waitingThreads.end(), std::this_thread::get_id()),
+                waitingThreads.end());
+        }
+
+      private:
+        Monitor<> sync_{};
+
+        const bool autoReset_;
+        bool flag_{false};
+        threads_ids_type waitingThreads_;
     };
-}
+}  // namespace utils
+#endif  // EVENT20_HPP
