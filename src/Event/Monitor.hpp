b57348a33d3cacefdf503e2356747ab7e878f28a
Damir Ljubic <60256258+damirlj@users.noreply.github.com>
2024-09-29 21:04:22 +0200

    Update Monitor.hpp

--- src/Event/Monitor.hpp
+++ src/Event/Monitor.hpp
@@ -33,11 +33,13 @@ class Monitor final
             explicit UnLockWithNotify(const Monitor& monitor) noexcept: monitor_{monitor}
             {}
             // Mutex interface
-            void lock() { monitor_.lock_.lock();}
-            void unlock() {
-                if constexpr (not broadcast) monitor_.condition_.notify_one();
-                else monitor_.condition_.notify_all();
-                monitor_.lock_.unlock();}
+            inline void lock() { monitor_.lock_.lock();}
+            inline void unlock() 
+            {
+                if constexpr (broadcast) monitor_.condition_.notify_all();
+                else monitor_.condition_.notify_one();
+                monitor_.lock_.unlock();
+            }
             private:
                 const Monitor& monitor_;
 
@@ -47,8 +49,8 @@ class Monitor final
     }
 
     /**
-     * Wait infinite on the predicate to be signaled, and return the locking 
-     * object to the client
+     * Wait infinite on the predicate to be signaled, and return 
+     * afterwards the locking object to the client
     */
     template <typename Predicate, typename... Args>
     requires std::is_same_v<bool, std::invoke_result_t<Predicate, Args...>>
@@ -72,7 +74,7 @@ class Monitor final
      *
      * Wait on the predicate to be signaled, or timeout being expired: whatever comes first,
      * and return the tuple containing the result of the wait operation and
-     * the locking object
+     * the locking object itself
     */
     template <typename Predicate, typename... Args>
     requires std::is_same_v<bool, std::invoke_result_t<Predicate, Args...>>
@@ -83,7 +85,7 @@ class Monitor final
     {
         std::unique_lock lock{lock_};
 
-        bool result = condition_.wait_for(
+        const bool result = condition_.wait_for(
             lock,
             timeout,
             [ p = std::forward<Predicate>(predicate), ...args = std::forward<Args>(args)]() mutable
@@ -117,12 +119,11 @@ class Monitor final
       auto notify(Func&& func, Args&&...args) const
       {
         auto lockWithNotify = getLockAndNotifyWhenDone<broadcast>();
-        std::unique_lock lock {lockWithNotify};
+        std::lock_guard lock {lockWithNotify};
         
         if constexpr (not std::is_void_v<std::invoke_result_t<Func, Args...>>)
         {
             return std::invoke(std::forward<Func>(func), std::forward<Args>(args)...);
-
         }
         
         std::invoke(std::forward<Func>(func), std::forward<Args>(args)...);
